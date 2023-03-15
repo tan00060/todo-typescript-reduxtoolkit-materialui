1fb4674085ca1c8270f50414875f9d4a2302f295
Michael DESKTOP <tan00060@algonquinlive.com>
2023-03-14 21:13:48 -0400

    removed old redux counter

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import todoListSlice from "../features/todoList/todoListSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     todoList: todoListSlice,
   },
 });
